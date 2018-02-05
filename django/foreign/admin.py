0a2c3e7c926d37acfd8269ac534ffc801a89cc2a
kahee <yuygh131@gmail.com>
2018-02-05 15:14:47 +0900

    foreignkey field에서 primary_key 를 따로 지정한 경우 실습

--- django/foreign/admin.py
+++ django/foreign/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from foreign.models import Manufacturer, Car
+from foreign.models import Manufacturer, Car, Person, Type, Pokemon
 
 admin.site.register(Manufacturer)
 admin.site.register(Car)
+admin.site.register(Person)
+admin.site.register(Type)
+admin.site.register(Pokemon)
--- django/foreign/models.py
+++ django/foreign/models.py
@@ -2,18 +2,52 @@ from django.db import models
 
 
 # Create your models here.
-class Manufacturer(models.Model):
-    name = models.CharField('제조사명',max_length=50)
-
-    def __str__(self):
-        return self.name
-
 
 class Car(models.Model):
-    manufacturer = models.ForeignKey(Manufacturer,
+    manufacturer = models.ForeignKey('Manufacturer',
                                      on_delete=models.CASCADE,
                                      verbose_name='제조사', )
     name = models.CharField(max_length=60)
 
     def __str__(self):
         return f'{self.manufacturer} {self.name}'
+
+
+class Manufacturer(models.Model):
+    name = models.CharField('제조사명', max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=60)
+    # 자기 자신을 다대일로 연결하는 필드
+    # 비어있어도 무관, 연결된 객체가 삭제되면 함께 삭제 되지 않음 .
+    # 해당 필드를 비움
+    teacher = models.ForeignKey(
+        'self',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Type(models.Model):
+    type_number = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return f'{self.type_number} |{self.name}'
+
+
+class Pokemon(models.Model):
+    dex_number = models.IntegerField(primary_key=True)
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.dex_number:03}, {self.name} {self.type.name}'
