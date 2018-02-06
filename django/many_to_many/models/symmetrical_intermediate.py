ee231602bef55b3b420b63e51bfad121bdb82b8d
kahee <yuygh131@gmail.com>
2018-02-06 14:45:12 +0900

    follow, block,following,block_users구현

--- django/many_to_many/models/symmetrical_intermediate.py
+++ django/many_to_many/models/symmetrical_intermediate.py
@@ -5,6 +5,7 @@ __all__ = (
     'Relation'
 )
 
+
 class TwitterUser(models.Model):
     '''
      내가 a를 follow 함
@@ -29,18 +30,47 @@ class TwitterUser(models.Model):
     def __str__(self):
         return self.name
 
+    @property
+    def block_user(self):
+        pk_list = self.relations_by_from_user.filter(
+            type=Relation.RELATION_TYPE_BLOCK).values_list('to_user', flat=True)
+
+        return TwitterUser.objects.filter(pk__in=pk_list)
+
+    @property
     def following(self):
         # 내가 from_user이며, type이 팔로잉인 Relation의 쿼리셋
-        following_relations  =  self.relations_by_from_user.filter(
-                type =Relation.RELATION_TYPE_FOLLOWING,
+        following_relations = self.relations_by_from_user.filter(
+            type=Relation.RELATION_TYPE_FOLLOWING,
         )
         # 위에서 정제한 쿼리셋에서 'to_user' 값만 리스트로 가져옴(내가 팔로잉하는 유저의 pk 리스트)
-        following_pk_list = following_relations.values_list('to_user',flat=True)
+        following_pk_list = following_relations.values_list('to_user', flat=True)
         # TwitterUser테이블에서 pk가
         # 바로 윗줄에서 만든 follwing_pk_list(내가 팔로잉하는 유저의 pk 리스트)
         # 에 포함하는 User목록을 following_users변수로 할당
-        following_users = TwitterUser.objects.filter(pk__in = following_pk_list)
+        following_users = TwitterUser.objects.filter(pk__in=following_pk_list)
         return following_users
+
+    def follow(self, to_user):
+        self.relations_by_from_user.filter(to_user=to_user).delete()
+
+        self.relations_by_from_user.create(
+            type=Relation.RELATION_TYPE_BLOCK,
+            to_user=to_user,
+        )
+
+        # Relation.objects.create(
+        #     from_user = self,
+        #     to_user = to_user,
+        #     type =  RELATION_TYPE_FOLLOWING
+        # )
+
+    def block(self, to_user):
+        self.relations_by_from_user.filter(to_user=to_user).delete()
+
+        self.relations_by_from_user.create(
+        type = Relation.RELATION_TYPE_BLOCK,
+        to_user = to_user, )
 
 
 class Relation(models.Model):
@@ -65,3 +95,14 @@ class Relation(models.Model):
         related_name='relations_by_to_user'
     )
     type = models.CharField(max_length=1, choices=CHOICES_TYPE)
+    # auto_now 업데이트 될때마다, auto_now_add 처음 불러왔을 때의 시간
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = (
+            # from_user와 to_user의 값이 이미 있을 경우
+            # DB에 중복 데이터 저장을 막음
+            # EX) from_user가 1, to_user가 3인 데이터가 이미 있다면 두 항목의 값이
+            # 모두 같은 또 다른 데이터가 존재 할 수 없음.
+            ('from_user', 'to_user'),
+        )
