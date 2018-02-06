722ea6afe1955a0156390d1c3019e41c2731dba7
kahee <yuygh131@gmail.com>
2018-02-06 12:24:02 +0900

    MTM에서 대칭이 False이며 중개모델을 쓰는 예제추가

--- django/many_to_many/models/symmetrical.py
+++ django/many_to_many/models/symmetrical.py
@@ -11,7 +11,14 @@ class InstagramUser(models.Model):
         'self',
         # 1번에서 2번 친구 2번에서 1번 친구는 자동이 아님 false
         symmetrical=False,
+        # 역참조시 사용할 이름
+        related_name= 'followers',
     )
 
     def __str__(self):
         return self.name
+
+    def followers(self):
+        # 자신을 following하고 있는 사람들을 출력
+        return self.followers.all()
+
---
+++ django/many_to_many/models/symmetrical_intermediate.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+
+class TwitterUser(models.Model):
+    '''
+     내가 a를 follow 함
+     나는 a의 follower
+     a는 나의 followee
+
+    a와 내가 서로 follow함
+        나와 a는 friend
+
+     Block기능이 있어야 함
+    '''
+
+    name = models.CharField(max_length=50)
+    relations = models.ManyToManyField(
+        # 관계에서 무언가를 가져와야하기 때문에 ,throuch로
+        'self',
+        symmetrical=False,
+        through='Relation',
+        related_name='+'
+    )
+
+    class Relation(models.Model):
+        RELATION_TYPE_FOLLOWING = 'f'
+        RELATION_TYPE_BLOCK = 'b'
+        CHOICES_TYPE = (
+            (RELATION_TYPE_FOLLOWING, '팔로잉'),
+            (RELATION_TYPE_BLOCK, '차단'),
+        )
+
+        from_user = models.ForeignKey(
+            TwitterUser,
+            on_delete=models.CASCADE,
+        )
+        to_user = models.ForeignKey(
+            TwitterUser,
+            on_delete=models.CASCADE,
+        )
+        type = models.CharField(max_length=1, choices=CHOICES_TYPE)
